a28dd5eeb01874f0a415c5c02afbaa953fb636ab
marcos3301 <39442784+marcos3301@users.noreply.github.com>
2018-05-21 14:21:44 -0300

    Update BuscaBinaria.java

--- BuscaBinaria.java
+++ BuscaBinaria.java
@@ -11,22 +11,57 @@ import java.util.Scanner;
 /**
  *
  * @author Marcos Antonio Silva Lima
- * 
+ *
  */
 public class BuscaBinaria {
 
     private static int iteracoes = 0;
     private static int meio = 0;
-    
+
     /**
-     * 
+     *
+     * @param vetor: referencia para o vetor instanciado na main..
+     * @param input: referencia para o objeto instanciado na main.
+     */
+    private static void preencherVetor(int[] vetor, Scanner input) {
+        int i = 0;
+        System.out.println("Digite o vetor de tamanho " + vetor.length + ", separando os elementos por espaço.");
+        while (i < vetor.length) {
+            vetor[i] = input.nextInt();
+            i++;
+        }
+    }
+
+    /**
+     *
+     * @param vetor: referencia para o vetor instanciado na main.
+     */
+    private static void ordenarVetor(int[] vetor) {
+        Arrays.sort(vetor);
+    }
+
+    /**
+     *
+     * @param vetor: referencia para o vetor instanciado na main.
+     */
+    private static void exibirVetor(int[] vetor) {
+        int i = 0;
+        while (i < vetor.length) {
+            System.out.print(vetor[i] + " ");
+            i++;
+        }
+    }
+
+    /**
+     *
      * @param vet, recebe o vetor onde sera feita a busca
      * @param valor, elemento a ser procurado no vetor
-     * @return retorna o numero de Iterações 
-     */   
+     * @return retorna o numero de Iterações
+     */
     private static int buscaNaoRecursiva(int[] vet, int valor) {
         int inicio = 0;
         int fim = vet.length - 1;
+        iteracoes++;
         meio = 0;
 
         while (inicio <= fim) {
@@ -44,7 +79,7 @@ public class BuscaBinaria {
     }
 
     /**
-     * 
+     *
      * @param vet, recebe o vetor onde será feita a busca.
      * @param inicio, define o inicio do vetor.
      * @param fim, define o final do vetor.
@@ -54,9 +89,9 @@ public class BuscaBinaria {
     private static int buscaRecursiva(int[] vet, int inicio, int fim, int valor) {
         iteracoes++;
         meio = (inicio + fim) / 2;
-        
+
         if (inicio > fim) {
-            return -1;
+            return iteracoes;
         } else if (valor == vet[meio]) {
             return iteracoes;
         } else if (valor < vet[meio]) {
@@ -74,29 +109,17 @@ public class BuscaBinaria {
         final int TAM = input.nextInt();
         int[] vetor = new int[TAM];
 
-        
-        int i = 0;
-        while (i < TAM) {
-            System.out.println("Digite um valor da posição " + i + " do vetor: ");
-            vetor[i] = input.nextInt();
-            i++;
-        }
-        Arrays.sort(vetor);
+        preencherVetor(vetor, input);
+        ordenarVetor(vetor);
+        ordenarVetor(vetor);
 
-        
-        i = 0;
-        while (i < TAM) {
-            System.out.print(vetor[i] + " ");
-            i++;
-        }
-        
-        
-        
-        iteracoes=0;
-        System.out.println("\nNº de Iterações na busca não recursiva: " + buscaNaoRecursiva(vetor, 10));
-        
-        iteracoes=0;
-        System.out.println("Nº de Iterações na busca recursiva: " + buscaRecursiva(vetor, 0, vetor.length - 1, 5));
+        iteracoes = 0;
+        int naoRecursiva = buscaNaoRecursiva(vetor, 10);
+        iteracoes = 0;
+        int recursiva = buscaRecursiva(vetor, 0, vetor.length - 1, 10);
+
+        System.out.println("\nNº de Iterações na busca não recursiva: " + naoRecursiva);
+        System.out.println("Nº de Iterações na busca recursiva: " + recursiva);
 
     }
 }
